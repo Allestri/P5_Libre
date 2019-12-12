d3091255c565ce9f1246d18ffde75e62ff9f63b6
Allestri <45837818+Allestri@users.noreply.github.com>
2019-12-12 05:58:39 +0100

    Added marker image GMap & updated Database

--- init_db.sql
+++ init_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 02 déc. 2019 à 02:00
+-- Généré le :  jeu. 12 déc. 2019 à 04:57
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -35,16 +35,7 @@ CREATE TABLE IF NOT EXISTS `avatars` (
   `avatar_file` varchar(255) NOT NULL,
   `active` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `avatars`
---
-
-INSERT INTO `avatars` (`id`, `user_id`, `avatar_file`, `active`) VALUES
-(2, 6, 'avatar_soku_10-15-07.jpg', 1),
-(3, 6, 'avatar_soku_10-41-11.jpg', 0),
-(8, 6, 'avatar_soku_06-29-59.jpg', 0);
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -62,17 +53,7 @@ CREATE TABLE IF NOT EXISTS `comments` (
   `moderated` int(11) NOT NULL DEFAULT '0',
   `post_id` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=245 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `comments`
---
-
-INSERT INTO `comments` (`id`, `author_id`, `content`, `com_date`, `reported`, `moderated`, `post_id`) VALUES
-(61, 6, 'Premier commentaire', '2019-11-19 22:02:54', 0, 0, 84),
-(123, 6, 'dqd', '2019-11-25 14:49:06', 0, 0, 86),
-(130, 6, '<b>Bonjour</b>', '2019-11-26 20:47:07', 0, 0, 86),
-(243, 6, 'sdfsf', '2019-11-30 19:57:11', 0, 0, 81);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -89,16 +70,7 @@ CREATE TABLE IF NOT EXISTS `friendship` (
   PRIMARY KEY (`id`),
   KEY `fk_friendship_to_members` (`friend_b`),
   KEY `fk_friendship_to_members2` (`friend_a`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `friendship`
---
-
-INSERT INTO `friendship` (`id`, `friend_a`, `friend_b`, `friend_date`) VALUES
-(6, 6, 18, '2019-09-06'),
-(8, 6, 19, '2019-09-06'),
-(9, 6, 22, '2019-11-26');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -115,14 +87,7 @@ CREATE TABLE IF NOT EXISTS `friend_requests` (
   PRIMARY KEY (`id`),
   KEY `uid` (`sender_id`),
   KEY `fk_members_to_friend_req2` (`receiver_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `friend_requests`
---
-
-INSERT INTO `friend_requests` (`id`, `sender_id`, `receiver_id`, `created_at`) VALUES
-(5, 6, 21, '2019-09-05 10:14:19');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -134,27 +99,17 @@ DROP TABLE IF EXISTS `images`;
 CREATE TABLE IF NOT EXISTS `images` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `filename` varchar(255) NOT NULL,
-  `height` int(11) NOT NULL,
-  `width` int(11) NOT NULL,
-  `size` int(11) NOT NULL,
-  `type` varchar(11) NOT NULL,
+  `height` int(11) NOT NULL DEFAULT '0',
+  `width` int(11) NOT NULL DEFAULT '0',
+  `size` int(11) NOT NULL DEFAULT '0',
+  `type` varchar(11) NOT NULL DEFAULT 'placeholder',
   `upload_date` datetime NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `groupimg_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `groupimg_id` int(11) NOT NULL DEFAULT '0',
   `privacy` int(11) DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `group_img` (`groupimg_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `images`
---
-
-INSERT INTO `images` (`id`, `filename`, `height`, `width`, `size`, `type`, `upload_date`, `user_id`, `groupimg_id`, `privacy`) VALUES
-(39, '99bed3f231eedcba.jpg', 2242, 3992, 6071612, 'image/jpeg', '2019-11-19 09:54:01', 6, 1, 0),
-(44, 'a002c4d2a573998a.JPG', 3000, 4000, 4606630, 'image/jpeg', '2019-11-23 08:33:02', 6, 1, 0),
-(46, '401601625eea000d.jpg', 2242, 3992, 5252806, 'image/jpeg', '2019-11-26 18:56:06', 6, 1, 0),
-(48, '17f647abbb29969d.JPG', 3000, 4000, 5232616, 'image/jpeg', '2019-11-26 18:58:02', 6, 1, 0);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -167,17 +122,7 @@ CREATE TABLE IF NOT EXISTS `images_groups` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `label` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `images_groups`
---
-
-INSERT INTO `images_groups` (`id`, `label`) VALUES
-(1, 'Drone'),
-(2, 'DLSR'),
-(3, 'Smartphone'),
-(4, 'Other');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -191,15 +136,7 @@ CREATE TABLE IF NOT EXISTS `likes` (
   `user_id` int(11) NOT NULL,
   `post_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=272 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `likes`
---
-
-INSERT INTO `likes` (`id`, `user_id`, `post_id`) VALUES
-(43, 6, 88),
-(271, 6, 81);
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -210,6 +147,11 @@ INSERT INTO `likes` (`id`, `user_id`, `post_id`) VALUES
 DROP TABLE IF EXISTS `logs`;
 CREATE TABLE IF NOT EXISTS `logs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `post_id` int(11) NOT NULL,
+  `post_name` varchar(255) NOT NULL,
+  `post_content` varchar(255) NOT NULL,
+  `author_id` int(11) NOT NULL,
+  `mod_type` varchar(255) NOT NULL DEFAULT 'none',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
@@ -230,17 +172,7 @@ CREATE TABLE IF NOT EXISTS `markers` (
   `image_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_image_to_markers` (`image_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `markers`
---
-
-INSERT INTO `markers` (`id`, `name`, `address`, `lng`, `lat`, `altitude`, `image_id`) VALUES
-(37, 'Douarnenez', 'placeholder', -4.28549, 48.108, 72.636, 39),
-(41, 'Quiberon', 'placeholder', -3.14242, 47.4902, 17.935, 44),
-(43, 'Guidel', 'placeholder', -3.507, 47.7474, 99.203, 46),
-(44, 'Larmor', 'placeholder', -3.37273, 47.7015, 85.81, 48);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -255,26 +187,12 @@ CREATE TABLE IF NOT EXISTS `members` (
   `password` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL DEFAULT 'email@default.com',
   `custom_avatar` tinyint(4) NOT NULL DEFAULT '0',
-  `avatar_file` varchar(255) NOT NULL,
   `group_id` tinyint(4) NOT NULL,
   `ip_address` varchar(50) DEFAULT NULL,
   `date` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_members_to_members_groups` (`group_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `members`
---
-
-INSERT INTO `members` (`id`, `name`, `password`, `email`, `custom_avatar`, `avatar_file`, `group_id`, `ip_address`, `date`) VALUES
-(6, 'SOku', '$2y$10$lOIr/VRtSKqwFFM5NoNJb.mMH9aoO2s1h97tzsDpFZmaCiSiuK59q', '', 1, 'avatar_default.png', 1, NULL, '2019-06-23 02:47:26'),
-(18, 'John', '$2y$10$UDVjA2pLHdZIfO64peq.W.QS3GlEVFeGkvUSKL7cg./Hb/ttB1qrW', 'email@default.com', 1, 'avatar_default.png', 2, '::1', '2019-09-05 06:04:44'),
-(19, 'Membre1', '$2y$10$qeOKW7srtV0iGFBfz7bBGuMckXoj5TddK290Zo/RL3NRXsTK4yHqW', 'email@default.com', 1, 'avatar_default.png', 3, '::1', '2019-09-05 06:30:48'),
-(20, 'Membre2', '$2y$10$RV21BhyVsqSLOiLoBCeWrOoStOhU7NVr5t57lZD0VvISc3c7HKlme', 'email@default.com', 1, 'avatar_default.png', 3, '::1', '2019-09-05 06:31:57'),
-(21, 'Membre3', '$2y$10$aPT267hpOs8skX3tGQyoduPpq6VFiBz.9EWVm2FxGjxbCzdWD2Mga', 'email@default.com', 1, 'avatar_default.png', 3, '::1', '2019-09-05 06:32:13'),
-(22, 'Private', '$2y$10$Ly4plL/wboZmuWYck.sSVu8itdXwGARhgBFbb2elWrR8tuYjsKvFW', 'email@default.com', 1, 'avatar_default.png', 3, '::1', '2019-09-08 10:08:38'),
-(23, 'Test56', '$2y$10$srbzRZu4WXMQgPfHb5WztekDG0tClMjpbe2XHOb7KF6BA57KxaFvi', 'email@default.com', 1, 'avatar_test56_17-08-37.gif', 3, '::1', '2019-10-01 19:02:18');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -287,16 +205,7 @@ CREATE TABLE IF NOT EXISTS `members_groups` (
   `id` tinyint(4) NOT NULL AUTO_INCREMENT,
   `label` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `members_groups`
---
-
-INSERT INTO `members_groups` (`id`, `label`) VALUES
-(1, 'Administrateur'),
-(2, 'Moderateur'),
-(3, 'Membre');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -309,23 +218,7 @@ CREATE TABLE IF NOT EXISTS `placeholder` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `content` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `placeholder`
---
-
-INSERT INTO `placeholder` (`id`, `content`) VALUES
-(1, 'Message 1'),
-(2, 'Message 2'),
-(3, 'Message 3'),
-(4, 'Message 4'),
-(5, 'Message 5'),
-(6, 'Message 6'),
-(7, 'Message 7'),
-(8, 'Message 8'),
-(9, 'Message 9'),
-(10, 'Message 10');
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -346,17 +239,7 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `privacy` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `image_id` (`image_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=91 DEFAULT CHARSET=utf8mb4;
-
---
--- Déchargement des données de la table `posts`
---
-
-INSERT INTO `posts` (`id`, `name`, `content`, `user_id`, `image_id`, `marker_id`, `liked`, `reported`, `privacy`) VALUES
-(81, 'Guidel', 'ModifiÃ©', 6, 39, 37, 50, 7, 0),
-(86, 'Quiberon', 'sfsf', 6, 44, 41, 1, 2, 0),
-(88, 'Guidel', 'Vue sur le MaÃ«va', 6, 46, 43, 0, 0, 0),
-(90, 'Larmor', 'Vue sur la baie de Lorient', 6, 48, 44, 1, 0, 0);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -370,22 +253,7 @@ CREATE TABLE IF NOT EXISTS `post_reports` (
   `user_id` int(11) NOT NULL,
   `post_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `post_reports`
---
-
-INSERT INTO `post_reports` (`id`, `user_id`, `post_id`) VALUES
-(1, 6, 81),
-(2, 6, 86),
-(3, 6, 23),
-(4, 6, 81),
-(5, 6, 81),
-(6, 6, 81),
-(7, 6, 81),
-(8, 6, 81),
-(9, 6, 81);
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -399,15 +267,6 @@ CREATE TABLE IF NOT EXISTS `privacy_categories` (
   `private_label` varchar(255) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `privacy_categories`
---
-
-INSERT INTO `privacy_categories` (`id`, `private_label`) VALUES
-(0, 'Public'),
-(1, 'Friends'),
-(2, 'User_only');
 
 --
 -- Contraintes pour les tables déchargées
